533a38a802d4a737246ae7771432ae426c292603
aguiluzv <aguiluzv@seattleu.edu>
2017-02-12 17:03:18 -0500

    Updated 2/12 @ 14:03

--- js/main.js
+++ js/main.js
@@ -16,6 +16,89 @@ $(document).on('ready', function(){
 
         // 1.   Accept a string value called `tags` as an argument. Example:
         //      `var searchPhotos = function(tags){`
+  
+  var searchImages = function(tags) {
+    
+        //
+        // 2.   Define the location of the Flickr API like this:
+        //      `var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";`
+    var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    console.log(tags);
+    $('#images').innerHTML = '<li class="search-throbber">Searching...</li>';    
+    
+        //
+        // 3.   Construct a `$.getJSON()` call where you send a request object
+        //      including the tags the user submitted, and a `done()` handler
+        //      that displays and refreshes the content appropriately.
+        //
+    
+   $.getJSON( flickrAPI, {
+      tags: tags,
+      tagmode: "any",
+      format: "json"
+    })  // getJSON call
+        // 4.   Update the display to add the images to the list with the id
+        //      `#images`.
+        .done(function( data ) {
+          
+          $("#images img").remove(); // remove previous images from page
+          $("#images ul").remove(); // remove previous data
+        
+          $.each( data.items, function( i, item ) {
+            //console.log(item.media.m);
+            console.log(item.title);
+            console.log(item.date_taken);
+            console.log(item.description);
+            console.log(item.author);
+            console.log(item.link);
+            
+            var image = item.media.m;
+            var title = item.title;
+            var date_taken = item.date_taken;
+            var description = item.description;
+            var author = item.author;
+            var link = item.link;
+            
+            //$( "<img>").append("<div class='col-xs-4'");
+            
+            //$( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+            
+            $( "#images" ).append( "<ul><li>" + "Title: " + title + "</li><li>" + "Date taken: "+ date_taken + "</li><li>" + "Description: "+ description + "</li><li>" + "Author: "+ author + "</li><li>" + "Link: "+ link + "</li></ul>");
+            
+            //<li><img src='" + image + "'></li>" +
+            
+            //<div class='col-xs-4'><ul>   </ul></div>
+            
+            //$( "<img>").append("</div>");
+            //$( "<img> li[i]").text("hi").appendTo("")
+            /*
+            if ( i === 3 ) {
+              return false;
+            }
+            */
+          });
+        });
+      
+    };
+  
+    $( "#flickr-button" ).click(function() {
+        event.preventDefault();  // prevent button from submitting
+
+        var tagName = document.getElementById('flickr-tag').value;  // get value of Flicker search input field
+
+        searchImages(tagName);  // execute searchImages function
+      });  //var searchImages
+  
+    
+  
+    // Create a function called `searchImages()`. This function will handle the
+    // process of taking a user's search terms and sending them to Flickr for a
+    // response.
+
+    // Inside the `searchImages()` function, the following things should happen:
+
+        // 1.   Accept a string value called `tags` as an argument. Example:
+        //      `var searchPhotos = function(tags){`
         //
         // 2.   Define the location of the Flickr API like this:
         //      `var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";`
@@ -46,4 +129,10 @@ $(document).on('ready', function(){
 
 
 
-});
+
+
+ 
+    
+
+}); // document.on
+
